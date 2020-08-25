f9c9423fa742a1ccd02a92cbc3f2df089634afc4
Damian Cop <cop.damian@gmail.com>
2020-08-25 17:33:45 +0200

    Add axios

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,8 +1,22 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import axios from 'axios';
 import '../styles/Home.css';
 import Navbar from '../components/Navbar';
 
 function Home() {
+
+    useEffect(() => {
+        axios
+        .get("https://swapi.dev/api/people/1")
+            .then(
+                (result) => {
+                    console.log(result);
+                }
+            )
+            .catch(err => {
+                console.log(err);
+            })
+    })
     return (
         <div className="Home">
             <Navbar />
